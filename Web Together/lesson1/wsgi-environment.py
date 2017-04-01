4152b9760199dab5114bed966aee4133148d2c19
Better <zhendie.bai@nokia.com>
2017-04-01 11:23:12 +0800

    WSGI code

--- Web Together/lesson1/wsgi-environment.py
+++ Web Together/lesson1/wsgi-environment.py
@@ -4,3 +4,22 @@ Created on 2017年3月31日
 
 @author: zhebai
 '''
+from wsgiref.simple_server import make_server
+
+def application(environ, start_response):
+    response_body = ['%s:%s' % (key, value) for key, value in sorted(environ.items())]
+    response_body = '\n'.join(response_body)
+    status = '200 OK'
+    response_headers = [('Content-Type','text/plain'),('Content-Length',str(len(response_body)))]
+    start_response(status, response_headers)
+    return [response_body.encode(encoding='utf_8', errors='strict')]
+
+#实例化WSGI server
+httpd = make_server(
+    '127.0.0.1',
+    8051,
+    application
+    )
+
+httpd.handle_request()
+print('end')
